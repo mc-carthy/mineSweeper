6582cbcfcea96446abc9e80d876a77d163394943
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-22 23:00:31 +0000

    Create main scene menu controller

---
+++ Assets/scripts/GameplayController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayController : MonoBehaviour {
+
+	public void RestartGame () {
+		SceneManager.LoadScene("main", LoadSceneMode.Single);
+	}
+
+	public void BackToMenu () {
+		SceneManager.LoadScene("menu", LoadSceneMode.Single);
+	}
+}
